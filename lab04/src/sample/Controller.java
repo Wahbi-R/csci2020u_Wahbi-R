e6bedfc78110ac15c343bb0ce839f4d6d1d5a5d6
Wahbi <wahbi.raihan@ontariotechu.net>
2021-02-24 04:51:03 -0500

    Fixing Phone Number Format

--- lab04/src/sample/Controller.java
+++ lab04/src/sample/Controller.java
@@ -3,13 +3,14 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML
     private TextField usernameField;
     @FXML
-    private TextField passwordField;
+    private PasswordField passwordField;
     @FXML
     private TextField fullNameField;
     @FXML
@@ -28,7 +29,7 @@ public class Controller {
         System.out.println("Password: " + this.passwordField.getText());
         System.out.println("Full Name: " + this.fullNameField.getText());
         System.out.println("E-Mail: " + this.emailField.getText());
-        System.out.println("Phone Number: " + this.phoneField.getText());
+        System.out.println("Phone Number: " + this.phoneField.getText().replaceFirst("(\\d{3})(\\d{3})(\\d+)", "$1-$2-$3"));
         System.out.println("Date of Birth: " + this.dateField.getValue());
     }
 }
