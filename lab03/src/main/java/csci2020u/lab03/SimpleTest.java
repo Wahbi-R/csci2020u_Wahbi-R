94053e1487f920603ef3930e0f131a2a7c543ef9
Wahbi <wahbi.raihan@ontariotechu.net>
2021-02-24 04:33:24 -0500

    Finished lab04

--- lab03/src/main/java/csci2020u/lab03/SimpleTest.java
+++ lab03/src/main/java/csci2020u/lab03/SimpleTest.java
@@ -1,7 +1,34 @@
-package csci2020u.lab03;
+final String ALGORITHM_NAME = "HmacSHA256";
+final String ENCODING_NAME = "UTF-8";
 
-public class SimpleTest {
-    public static void main(String[] args){
-        System.out.println("Hello friends!");
-    }
-}
+final String CLIENT_ID = "9c0d9e12-48fe-451a-87b2-827f11c46f12";
+final String CLIENT_SECRET = "b88d0646-0d7f-4c6c-a3ff-39ea34ceb342";
+
+String currentTime = Long.toString(System.currentTimeMillis() / 1000L);
+
+/*
+  If the request has a body, you would use this instead of empty string below (replace BODY with actual request body):
+
+  MessageDigest digest = MessageDigest.getInstance("SHA-256");
+  byte[] encodedHash = digest.digest("BODY".getBytes(StandardCharsets.UTF_8));
+  String hashed_body = Hex.encodeHexString(encodedHash);
+*/
+
+String[] signatureParameters = {
+    "GET",
+    "",
+    "/api/PATH",
+    "",  //If the request has a BODY, this would be hashed_body
+    currentTime
+};
+
+String signatureData = String.join(":", signatureParameters);
+
+final Mac sha256_HMAC = Mac.getInstance(ALGORITHM_NAME);
+final SecretKeySpec secret_key = new SecretKeySpec(CLIENT_SECRET.getBytes(ENCODING_NAME), ALGORITHM_NAME);
+sha256_HMAC.init(secret_key);
+
+byte[] computedSignature = sha256_HMAC.doFinal(signatureData.getBytes(ENCODING_NAME));
+
+String NewtonAPIAuth = CLIENT_ID + ":" + Base64.getEncoder().encodeToString(computedSignature);
+String NewtonDate = currentTime;
---
+++ lab04/src/sample/Controller.java
@@ -0,0 +1,34 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private TextField fullNameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField phoneField;
+    @FXML
+    private DatePicker dateField;
+
+    public Controller() {
+    }
+
+    @FXML
+    protected void registerAction(ActionEvent event) {
+        System.out.println("Username: " + this.usernameField.getText());
+        System.out.println("Password: " + this.passwordField.getText());
+        System.out.println("Full Name: " + this.fullNameField.getText());
+        System.out.println("E-Mail: " + this.emailField.getText());
+        System.out.println("Phone Number: " + this.phoneField.getText());
+        System.out.println("Date of Birth: " + this.dateField.getValue());
+    }
+}
